feb1e16f8e53233034299693814f9b13b023da48
HERITAGE-XION <iheritage934@gmail.com>
2025-12-16 19:36:02 +0100

    added types folder and transferred all the types into the following there respective types file - cart-store - cart-provider -products

---
+++ types/product.ts
@@ -0,0 +1,27 @@
+// Types
+export interface CartItem {
+	id: number;
+	name: string;
+	variant: string;
+	price: number;
+	quantity: number;
+	image: string;
+	inStock: boolean;
+}
+
+export interface ProductColor {
+	name: string;
+	value: string;
+	bgClass: string;
+}
+
+export interface ProductSpec {
+	label: string;
+	value: string;
+}
+
+export interface ProductImage {
+	id: number;
+	src: string;
+	alt: string;
+}
