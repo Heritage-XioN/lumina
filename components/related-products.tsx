ed03f667f95664235051751e0c03b29e9898c236
HERITAGE-XION <iheritage934@gmail.com>
2025-12-17 14:59:10 +0100

    fixed a prop issue

--- components/related-products.tsx
+++ components/related-products.tsx
@@ -7,9 +7,9 @@ const RelatedProducts = () => {
   return (
     // displays the related products on the products details page
     <ScrollArea className="w-full rounded-md border whitespace-nowrap">
-      <div className="flex w-max gap-4 space-x-4 p-4">
+      <div className="flex w-max gap-6 space-x-4 p-4">
         {productList.map((items) => (
-          <ProductCard key={items.id} {...items} image1={items.images[0].src} image2={items.images[1].src} />
+          <ProductCard key={items.id} {...items} image1={items.images[0].src} image2={items.images[1].src} variant={items.colors[0].name}/>
         ))}
       </div>
       <ScrollBar orientation="horizontal" />
